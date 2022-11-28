ae3b72883e2a0bfddfcd9b8fef1a218e846ceaea
Obaid <obaid3035@gmail.com>
2022-11-28 22:10:07 +0500

    Stack

---
+++ data-structure/stack.ts
@@ -0,0 +1,48 @@
+import {ListType} from "./singly-linked-list";
+
+class StackNode {
+    constructor(
+        public value: ListType,
+        public next: StackNode = null
+    ) {
+    }
+}
+
+class Stack {
+    constructor(
+        public first: StackNode = null,
+        public last: StackNode = null,
+        public size: number = 0
+    ) {
+    }
+
+    push(val: ListType) {
+        const newNode = new StackNode(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+
+    pop() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        return temp.value;
+    }
+}
+
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.pop();
